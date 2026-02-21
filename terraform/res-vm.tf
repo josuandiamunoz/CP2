96d577bed4ce7068e2e63b15ac0e300c0b86a64d
Josu Andia <andiajosu@gmail.com>
2026-02-21 11:32:17 +0100

    Resource files for virtual machine

---
+++ terraform/res-vm.tf
@@ -0,0 +1,27 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-terraform"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2s_v3"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  default = "Norway East"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
